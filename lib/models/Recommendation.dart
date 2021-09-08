36ab37547e7146ce11a8c95119c229ddc338b177
matheus_andrade <mthrafaelandrade@gmail.com>
2021-09-07 21:06:11 -0300

    some changes

--- lib/models/Recommendation.dart
+++ lib/models/Recommendation.dart
@@ -9,13 +9,13 @@ final List<Recommendation> recommendations = [
     name: "Kolapo Obanewa",
     source: "Linkedin",
     text:
-        "I have had the pleasure of working with Anwar on two flutter projects and what strikes me the most about his work is his ability to write clean and reusable codes with best practices. He is hardworking and makes it so easy to transcribe ideas into beautiful and testable flutter apps. He is a Flutter/Dart gem and has my highest recommendation",
+        "I have had the pleasure of working with Matheus on two flutter projects and what strikes me the most about his work is his ability to write clean and reusable codes with best practices. He is hardworking and makes it so easy to transcribe ideas into beautiful and testable flutter apps. He is a Flutter/Dart gem and has my highest recommendation",
   ),
   Recommendation(
     name: "Reza Shahbazi",
     source: "Linkedin",
     text:
-        "Abu is great asset for any organization. It's a true pleasure working with him at TakeIn. His flutter skill is amazing as well as his professionalism and being a good team player. Abu's problem solving skill is also one of his great skills.",
+        "Matheus is a great asset for any organization. It's a true pleasure working with him at TakeIn. His flutter skill is amazing as well as his professionalism and being a good team player. Abu's problem solving skill is also one of his great skills.",
   ),
   Recommendation(
     name: "Diadem",
@@ -27,6 +27,6 @@ final List<Recommendation> recommendations = [
     name: "Roshan Shetty",
     source: "YouTube",
     text:
-        "Very straightforward, professional and also the best flutter videos in the youtube! It will be great if you add some comments to your steps with 0.5 seconds pause before implementing this step. By meaning of steps, I mean not the basic, but structural steps, like 10-20 steps per video. However, thank you very much!",
+        "Very straightforward, professional and also the best flutter developer! It will be great if you add some comments to your steps with 0.5 seconds pause before implementing this step. By meaning of steps, I mean not the basic, but structural steps, like 10-20 steps per video. However, thank you very much!",
   ),
 ];
